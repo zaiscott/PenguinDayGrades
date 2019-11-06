3204fee5319ad47d46f279d19419a7ab0a924a62
unknown <you@example.com>
2019-11-06 13:09:27 -0500

    1:10 10/30

--- java.js
+++ java.js
@@ -1,30 +1,34 @@
-var penPromise= d3.json("classData.json")
+var penPromise= d3.json("penguins/classData.json")
 penPromise.then(function(penData){
     console.log("penData", penData);
+  var final=  getGrade(penData)
+  console.log(final)
 },
     function(err)
 {
     console.log("fail", err)
 })
 //create an array of quizes
-var getGrade =function(quizes)
+var getGrade =function(penData)
     {
-    var quizGrades= quizes.map(function(quiz) 
+    var quizGrades= penData.map(function(penguin) 
         {
-        return quiz.grade
+        return penguin.quizes[0].grade
+        
         }
 
     )
     return quizGrades; }
 
+
 //Create SVG element
-var svg = d3.select("body")
-            .append("svg")
-            .attr("width", 500)
+//var svg = d3.select("body")
+//            .append("svg")
+/*            .attr("width", 500)
             .attr("height", 500);
 
 svg.selectAll("circle")
-   .data(getGrade)
+   .data(getGrade())
    .enter()
    .append("circle")
 
@@ -34,4 +38,4 @@ svg.selectAll("circle")
    .attr("cy", function(d) {
         return d[1];
    })
-   .attr("r", 5);
+   .attr("r", 5);*/
