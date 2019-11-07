bbb19f784517c0116b303369fa010850f4a5b71b
Zai <54860537+zaiscott@users.noreply.github.com>
2019-11-06 23:31:44 -0500

    Update java.js

--- java.js
+++ java.js
@@ -1,41 +1,102 @@
-var penPromise= d3.json("penguins/classData.json")
-penPromise.then(function(penData){
-    console.log("penData", penData);
-  var final=  getGrade(penData)
-  console.log(final)
-},
-    function(err)
+var penPromise = d3.json("penguins/classData.json");
+
+
+
+
+var Days = d3.range(1,39);
+
+
+
+
+
+penPromise.then(
+function(penguins)
 {
-    console.log("fail", err)
-})
-//create an array of quizes
-var getGrade =function(penData)
+    
+    console.log(penguins);
+    
+    // Ok data is grabbed, time to really start the lab ;)
+    
+     Days.forEach(function(dayNum, index) // needs two parameters for buttons and indexing
     {
-    var quizGrades= penData.map(function(penguin) 
-        {
-        return penguin.quizes[0].grade
-        
-        }
+        d3.select("body")
+          .append("button")
+          .text("Day: " + dayNum)
+         
+         // Made the buttons based off the variable (Days).
+         
+          .on("click", function()
+            {
+                plotPoints(penguins, index); // finds data from given penguin and plots it
+                d3.select("p").text("Visualization for Day " + dayNum); // Labels  the Graph for each button
+            })
+            
+    })
+   
+},
+function(err)
+{
+    console.log("fail", err);
+})
 
-    )
-    return quizGrades; }
 
 
-//Create SVG element
-//var svg = d3.select("body")
-//            .append("svg")
-/*            .attr("width", 500)
-            .attr("height", 500);
 
-svg.selectAll("circle")
-   .data(getGrade())
-   .enter()
-   .append("circle")
 
-.attr("cx", function(d) {
-        return d[0];
-   })
-   .attr("cy", function(d) {
-        return d[1];
-   })
-   .attr("r", 5);*/
+
+
+// Put functions to use in Promise down here
+
+var screen = {height: 600, width: 800}; // svg screen, tells it how big it is
+
+var drawGraph = function(points, xAxis, yAxis)
+{
+    d3.selectAll("svg *").remove();
+    
+    d3.select("svg")
+        .selectAll("circle")
+        .data(points)
+        .enter()
+        .append("circle")
+        .attr("cx", function(p){return xAxis(p.x)})
+        .attr("cy", function(p){return yAxis(p.y)})
+        .attr("r", 5);
+}
+
+var plotPoints = function(penguins, indexDay) 
+{
+    var points = penguins.map(function(d, i) // needs to look at every position in the array and return the day and grade being asked for in order to plot
+    {
+        var yData = d.quizes[indexDay].grade
+        var xData = i
+        return {x: xData, y: yData};
+    })
+    
+    d3.select("svg")
+        .attr("height", screen.height)  
+        .attr("width", screen.width);
+    
+    var xAxis = d3.scaleLinear()
+    
+    xAxis.domain(
+        [
+            d3.min(points, function(d){return d.x;}), 
+            d3.max(points, function(d){return d.x;})
+        ]);
+    
+    xAxis.range([0,screen.width]);
+    
+    var yAxis = d3.scaleLinear()
+    
+    yAxis.domain(
+        [
+            d3.min(points, function(d){return d.y;}), 
+            d3.max(points, function(d){return d.y;})
+        ]);
+    
+    yAxis.range([screen.height,0]);
+    
+    drawGraph(points, xAxis, yAxis);
+}
+
+
