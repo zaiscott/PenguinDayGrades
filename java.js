5fde787a5de9dad0f52fb39134a2686f2cfce569
selamvv <selam.vanvoorhis@centre.edu>
2019-11-11 14:21:26 -0500

    js updated

--- java.js
+++ java.js
@@ -1,41 +1,79 @@
 var penPromise= d3.json("penguins/classData.json")
 penPromise.then(function(penData){
+
+  var EachDay= d3.range(39) 
     console.log("penData", penData);
-  var final=  getGrade(penData)
+  var final=  getGrade(penData, 1)
   console.log(final)
+   console.log(dataset(final))
+    setup(dataset(final))
+EachDay.forEach(function(num){
+    AllDays(penData, num)
+})
+
+   
 },
     function(err)
-{
-    console.log("fail", err)
-})
-//create an array of quizes
-var getGrade =function(penData)
     {
+        console.log("fail", err)
+    })
+var AllDays= function(penData, day){
+     d3.select("body")
+.append("button")
+.text("day" + day)
+.on("click", function(){
+   var final=getGrade(penData, day)
+   d3.selectAll("svg *").remove()
+   setup(dataset(final))
+
+    })
+}
+//create an array of quizes
+var getGrade =function(penData, day) {
     var quizGrades= penData.map(function(penguin) 
         {
-        return penguin.quizes[0].grade
-        
-        }
-
-    )
+        return penguin.quizes[day].grade
+       
+        })
     return quizGrades; }
 
+//create the points, array of penguin and grade objects 
+var dataset= function(final){
+     
+  var points= final.map(function(quiz, i){
+        return {x: i,
+                y: quiz}
+    })
+  return points}
 
-//Create SVG element
-//var svg = d3.select("body")
-//            .append("svg")
-/*            .attr("width", 500)
-            .attr("height", 500);
+ //function to display the grades for each day
+ 
 
-svg.selectAll("circle")
-   .data(getGrade())
-   .enter()
-   .append("circle")
 
-.attr("cx", function(d) {
-        return d[0];
-   })
-   .attr("cy", function(d) {
-        return d[1];
-   })
-   .attr("r", 5);*/
+//set up the screen
+ var setup= function(points){
+    var xScale= d3.scaleLinear()
+        xScale.domain([0,22])
+    xScale.range([screen.width,0])
+    
+    var yScale= d3.scaleLinear()
+        yScale.domain([10,0])
+    yScale.range([screen.width,0])
+    drawPoints(points, xScale, yScale)
+ }
+
+ var screen = {width: 900, height: 700}
+ 
+ //bind the points onto the screen
+ var drawPoints= function(points, xScale, yScale){
+     d3.select("svg")
+     .attr("height", screen.height)
+     .attr("width", screen.width)
+     .selectAll("circle")
+     .data(points)
+     .enter()
+     .append("circle")
+     .attr("cx", function(p){return xScale(p.x)})
+     .attr("cy", function(p){return yScale(p.y)})
+     .attr("r", 20)
+ }              
